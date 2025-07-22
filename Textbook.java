c586821d7aa8e32dda3ee4da30b35bf318ca12b1
reem khalifa <aladawi12reem6@gmail.com>
2025-07-22 23:28:22 +0400

    Add Textbook class extending Book class

--- Textbook.java
+++ Textbook.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+public class Textbook extends Book {
+    private String subject;
+    private int edition;
 
-public class Textbook {
+    public Textbook(String title, String author, int pages, String subject,
+                    int edition) {
+        // Constructor that calls parent constructor
+        super(title,author,pages);
+        // Initialize subject and edition
+        this.subject=subject;
+        this.edition=edition;
+    }
+    // Override the displayInfo method
+    @Override
+    public void displayInfo() {
+        // Call parent's displayInfo using super.displayInfo()
+        super.displayInfo();
+        // Then add subject and edition information
+        System.out.println("Subject:"+" " + subject);
+        System.out.println("Edition: "+" " + edition);
+
+
+    }
+// Add getter methods for subject and edition
+    public String getSubject() {return subject;}
+    public int getEdition() {return edition;}
 }
